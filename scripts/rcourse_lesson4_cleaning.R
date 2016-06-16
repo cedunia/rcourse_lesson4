789a553f2e99d7c8991191bc053329a92aa8171a
cedunia <cedric.hassenkhodja@gmail.com>
2016-06-16 12:08:24 +0200

    made cleaning script

---
+++ scripts/rcourse_lesson4_cleaning.R
@@ -0,0 +1,26 @@
+install.packages('purrr')
+
+## LOAD PACKAGES ##
+library(dplyr)
+library(purrr)
+
+## READ IN DATA ##
+data <- list.files(path = 'data', full.names = TRUE) %>%
+  map(read.table, header = TRUE, sep = "\t", na.strings = c("",NA)) %>%
+  reduce(rbind)
+
+## CLEAN DATA ##
+data_clean <- data %>%
+  filter(series != "tas") %>%
+  mutate(series = factor(series)) %>%
+  filter(alignment == 'foe' | alignment == 'friend') %>%
+  mutate(alignment = factor(alignment)) %>%
+  filter(!is.na(conservation)) %>%
+  mutate(extinct = ifelse(conservation == "LC",0,1)) %>%
+  group_by(series, alignment, alien) %>%
+  arrange(episode) %>%
+  filter(row_number() == 1) %>%
+  ungroup()
+
+
+
